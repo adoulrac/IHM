def23804f6b8462e899c60d192690beeadb31931
RACHID ADOUL <ra.adoul@gmail.com>
2014-11-12 18:04:51 +0000

    Comment interface methods.

--- src/app/interfaces/DataToIhm.java
+++ src/app/interfaces/DataToIhm.java
@@ -6,11 +6,11 @@ import java.util.List;
  * Created by greyna on 11/12/14.
  */
 public interface DataToIhm {
-    void receiveUser(User user, int queryId);
+/*    void receiveUser(User user, int queryId);
     void receiveUsers(List<User> users, int queryId);
     void receivePicture(Picture picture, int queryId);
     void receivePictures(List<Picture> pictures, int queryId);
     void receiveComment(Comment comment, int queryId);
     void receiveNote(Note note, int queryId);
-    void receiveFriendRequest(User user, int queryId);
+    void receiveFriendRequest(User user, int queryId);*/
 }
