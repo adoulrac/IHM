4a79cb8f81a2c74db232fce2995d820c2c9d16ef
8-Miles <selim.zenagui@gmail.com>
2014-11-19 19:53:40 +0100

    Modif. welcome

--- controller/WelcomeController.java
+++ controller/WelcomeController.java
@@ -1,13 +1,33 @@
 package IHM.controller;
 
+import java.util.ResourceBundle;
+
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class WelcomeController implements Initializable {
+public class WelcomeController implements Initializable{
 
     private MainController application;
+
+    @FXML
+    private Button BtnDeconnect;
+    @FXML
+    private Button BtnGroupsHandling;
+    @FXML
+    private Button BtnPreferences;
+    @FXML
+    private Button BtnAddFriends;
+
+
+    public WelcomeController()
+    {
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -17,4 +37,5 @@ public class WelcomeController implements Initializable {
     public void setApp(MainController application){
         this.application = application;
     }
+
 }
