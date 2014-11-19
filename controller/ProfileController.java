bcf5b493c20a5641d96c894f8fe36b0a410171b3
RACHID ADOUL <ra.adoul@gmail.com>
2014-11-19 18:18:38 +0000

    Login feature

--- controller/ProfileController.java
+++ controller/ProfileController.java
@@ -1,5 +1,21 @@
 package IHM.controller;
 
-public class ProfileController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
+
+    private MainController application;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setApp(MainController application){
+        this.application = application;
+    }
 
 }
