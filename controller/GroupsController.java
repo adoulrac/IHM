995a910f36a38639a74373cabb339538989c0e81
8-Miles <selim.zenagui@gmail.com>
2014-11-20 09:22:24 +0100

    Launch groups from welcome

--- controller/GroupsController.java
+++ controller/GroupsController.java
@@ -1,4 +1,22 @@
 package IHM.controller;
 
-public class GroupsController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GroupsController implements Initializable
+{
+
+    private MainController application;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setApp(MainController application){
+        this.application = application;
+    }
+
 }
--- controller/WelcomeController.java
+++ controller/WelcomeController.java
@@ -2,40 +2,45 @@ package IHM.controller;
 
 import java.util.ResourceBundle;
 
+import IHM.Main;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.Scene;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class WelcomeController implements Initializable{
+public class WelcomeController extends Pane implements Initializable {
 
     private MainController application;
 
     @FXML
-    private Button BtnDeconnect;
+    private Button btnDeconnect;
     @FXML
-    private Button BtnGroupsHandling;
+    private Button btnGroupsHandling;
     @FXML
-    private Button BtnPreferences;
+    private Button btnPreferences;
     @FXML
-    private Button BtnAddFriends;
-
-
-    public WelcomeController()
-    {
-
-    }
+    private Button btnAddFriends;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(final URL url, final ResourceBundle resourceBundle) {
         //TODO
     }
 
-    public void setApp(MainController application){
-        this.application = application;
+    public void setApp(final MainController app) {
+        this.application = app;
+    }
+
+    public void launchPreferences() {
+        this.application.goToProfile();
+    }
+
+    public void launchGroups() {
+       this.application.goToGroups();
     }
 
 }
